72fb2340e0868d960ff428c7d32a6cf0a3d3b6e2
imoghul <moghul02@gmail.com>
2023-11-22 16:21:25 +0000

    added command line arguments

---
+++ README.md
@@ -0,0 +1,22 @@
+# MAP Analyzer Script
+
+## Setup
+To use the script, go to the e2studio build steps settings and add the command under post-build steps.
+An example is shown below:
+``` bash
+python "absolute/script/path" --rom ".text" --ram ".bss" ".data" --identifiers "./src/" "./QCIOT009"
+```
+## Usage
+To use the script, make sure you have built the project and exported the csv files for usage under the "Memory Usage" view in e2studio. Then, after building again, you will see the usage
+### Arguments
+
+* --identifiers: any path that contains one of the identifiers will be looked at. All others are ignored
+* --rom : These are all the labels that are under rom
+* --ram : These are all the labels that are under ram
+
+Keep in mind arguments aren't required and are defaulted to:
+- --identifiers : "./src/"
+- --rom : ".text"
+- --ram : ".bss" ".data"
+
+Note: If an entry's label doesn't match rom nor ram, it will throw an error. Make sure your rom and ram labels cover all possibilities
--- main.py
+++ main.py
@@ -1,17 +1,59 @@
 import csv
+import sys
 
-labels = ""
-with open('labels.txt', 'r') as f:
-  labels = f.read().split("\n")
+sections = sys.argv
 
+romSectionIndex = -1
+romSectionEnd = -1
+ramSectionIndex = -1
+ramSectionEnd = -1
+identifiersIndex = -1
+identifiersEnd = -1
+try:
+  romSectionIndex = (sections.index("--rom"))
+except ValueError:
+  pass
+
+try:
+  ramSectionIndex = (sections.index("--ram"))
+except ValueError:
+  pass
+
+try:
+  identifiersIndex = (sections.index("--identifiers"))
+except ValueError:
+  pass
+
+indeces = [romSectionIndex, ramSectionIndex, identifiersIndex]
+indeces.sort()
+ends = [-1, -1, -1]
+for i, v in enumerate(indeces[:-1]):
+  if v != -1:
+    ends[i] = indeces[i + 1]
+ends[-1] = len(sys.argv)
+
+romSectionLocation = indeces.index(romSectionIndex)
+ramSectionLocation = indeces.index(ramSectionIndex)
+identifiersLocation = indeces.index(identifiersIndex)
+
+romSections = sections[indeces[romSectionLocation] +
+                       1:ends[romSectionLocation]] if indeces[
+                           romSectionLocation] != -1 else ['.text']
+ramSections = sections[indeces[ramSectionLocation] +
+                       1:ends[ramSectionLocation]] if indeces[
+                           ramSectionLocation] != -1 else ['.data', '.bss']
+identifiers = sections[indeces[identifiersLocation] +
+                       1:ends[identifiersLocation]] if indeces[
+                           identifiersLocation] != -1 else ["./src/"]
+
+print("Using these identifiers: ", identifiers)
+print("Using these rom sections: ", romSections)
+print("Using these ram sections: ", ramSections)
 # romEnd = 0x20000
 # ramStart = 0x20000000
 
 # any paths that contain any of these will be counted
-identifiers = ['./QCIOT009', './src/']
-
-romSections = ['.text']
-ramSections = ['.bss', '.data']
+# this will usually be the api and src directories
 
 
 def isRom(start, end, section, group):
@@ -56,8 +98,8 @@ def readObjectFile(filename, addrIndexedList):
             # print(row)
             # print(start, int(row[2], 0), inRom, inRam)
             raise Exception(
-                f"Update the ram and rom detection functions, this line doesn't match either: {row}"
-            )
+                "Update the ram and rom detection functions, this line doesn't match either: "
+                + str(row))
 
           assert (inRom != inRam)
           if inRom:
@@ -103,8 +145,8 @@ def readSymbolFile(filename, addrIndexedList):
             # print(row)
             # print(start, int(row[2], 0), inRom, inRam)
             raise Exception(
-                f"Update the ram and rom detection functions, this line doesn't match either: {row}"
-            )
+                "Update the ram and rom detection functions, this line doesn't match either: "
+                + str(row))
           assert (inRom != inRam)
           if inRom:
             romUsage += int(row[3])
@@ -128,15 +170,15 @@ def readSymbolFile(filename, addrIndexedList):
   return romUsage, ramUsage
 
 
-print("\t\t\t\t\t rom\tram")
-for file in ["master freertos", "master noos", "slave freertos", "slave noos"]:
+print("               \t\t  rom\tram")
+for file in ["."]:
   addrIndexedList = {}
   print(file)
   romUsage = 0
   ramUsage = 0
-  romUsage, ramUsage = readObjectFile(f"{file}/Object.csv", addrIndexedList)
+  romUsage, ramUsage = readObjectFile("%s/Object.csv" % file, addrIndexedList)
   print("Object file only:\t", romUsage, "\t", ramUsage)
-  romUsage, ramUsage = readSymbolFile(f"{file}/Symbol.csv", addrIndexedList)
+  romUsage, ramUsage = readSymbolFile("%s/Symbol.csv" % file, addrIndexedList)
   print("Symbol file only:\t", romUsage, "\t", ramUsage)
 
   romUsage = 0
@@ -145,5 +187,5 @@ for file in ["master freertos", "master noos", "slave freertos", "slave noos"]:
     if (addrIndexedList[i][-1]): ramUsage += addrIndexedList[i][0]
     elif (addrIndexedList[i][-2]): romUsage += addrIndexedList[i][0]
 
-  print("Both files:\t\t\t", romUsage, "\t", ramUsage)
+  print("Both files:      \t", romUsage, "\t", ramUsage)
   print()
